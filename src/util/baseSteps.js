5eaed2268fa9ff2dabd03b9fcb62b2fcc10256a7
Lucas123zx <lucas123zk@live.com>
2024-07-26 11:57:46 -0300

    adicionados teste revisados

--- src/util/baseSteps.js
+++ src/util/baseSteps.js
@@ -4,21 +4,33 @@ const { spec } = require('pactum');
 class BaseSteps {
 
   static async cadastraLivro(urlBook,livro) {
-    return await spec()
+     let cadastra = await spec()
       .post(urlBook)
       .withHeaders('Content-Type', 'application/json')
       .withBody(livro);
+
+    let resposta = {json: cadastra.json, statusCode: cadastra.statusCode, statusMessage:cadastra.statusMessage};
+
+    return resposta;
   };
 
   static async consultaLivro(urlBook, id){
-    return await spec()
+    let consulta = await spec()
       .get(urlBook + `/${id}`);
+
+    let resposta = {json: consulta.json, statusCode: consulta.statusCode, statusMessage:consulta.statusMessage}; 
+    return resposta;  
   };
 
-  static respostaOK(resposta, livro) {
-      expect(resposta.statusCode).to.be.eql(200);
-      expect(resposta.statusMessage).to.be.eql('OK');
-      expect(resposta.json).to.be.eql(livro);
+  static async DeletaLivro(urlBook, id){
+    return await spec()
+      .delete(urlBook + `/${id}`);
+  };
+
+  static respostaOK(resposta , livro) {
+    expect(resposta.statusCode).to.be.eql(200);
+    expect(resposta.statusMessage).to.be.eql('OK');
+    expect(resposta.json).to.be.eql(livro);
   };
 
   static respostaOKEArrayNaoVazio(resposta) {
--- tests/books.js
+++ tests/books.js
@@ -2,7 +2,8 @@ const { spec } = require('pactum');
 const { expect } = require("chai");
 require('dotenv').config();
 const Livro = require('../src/util/livro');
-const BaseSteps = require('../src/util/baseSteps')
+const BaseSteps = require('../src/util/baseSteps');
+const { afterEach, after } = require('mocha');
 
 describe("Recurso Book", async () => {
 
@@ -20,11 +21,9 @@ describe("Recurso Book", async () => {
     "publishDate": meuLivro.publishDate
   };
 
-  before("Criar um livro", async () => {
-    BaseSteps.cadastraLivro(urlBook,livro);
-  });
-
   it('Recuperar todos os livros com sucesso', async () => {
+    await BaseSteps.cadastraLivro(urlBook,livro);
+    
     let consulta = await spec().get(urlBook);
     resposta = {json: consulta.json, statusCode: consulta.statusCode, statusMessage:consulta.statusMessage};
 
@@ -32,57 +31,55 @@ describe("Recurso Book", async () => {
   });
 
   it('Cria um livro com sucesso', async () => {
-    let cadastra = await BaseSteps.cadastraLivro(urlBook, livro);
-    resposta = {json: cadastra.json, statusCode: cadastra.statusCode, statusMessage:cadastra.statusMessage};
+    resposta = await BaseSteps.cadastraLivro(urlBook, livro);
     BaseSteps.respostaOK(resposta, livro);
 
-    let consulta = await BaseSteps.consultaLivro(urlBook, resposta.json.id);
-    resposta = {json: consulta.json, statusCode: consulta.statusCode, statusMessage:consulta.statusMessage};
+    resposta = await BaseSteps.consultaLivro(urlBook, resposta.json.id)
     BaseSteps.respostaOK(resposta, livro);
   });
 
   it('Cria um livro sem informar titulo', async () => {
     livro.title = "";
-    let cadastra = await BaseSteps.cadastraLivro(urlBook, livro);
-    resposta = {json: cadastra.json, statusCode: cadastra.statusCode, statusMessage:cadastra.statusMessage};
+
+    resposta = await BaseSteps.cadastraLivro(urlBook, livro);
+    BaseSteps.respostaOK(resposta, livro);
 
     expect(resposta.statusCode).to.be.eql(200);
     expect(resposta.statusMessage).to.be.eql('OK')
     expect(resposta.json.title).to.be.empty;
     expect(resposta.json).to.be.eql(livro);
 
-    let consulta = await BaseSteps.consultaLivro(urlBook, resposta.json.id)
-    resposta = {json: consulta.json, statusCode: consulta.statusCode, statusMessage: consulta.statusMessage};
+    resposta = await BaseSteps.consultaLivro(urlBook, resposta.json.id)
     BaseSteps.respostaOK(resposta, livro);
   });
 
   it('Cria um livro sem informar descrição', async () => {
     livro.description = "";
-    let cadastra = await BaseSteps.cadastraLivro(urlBook, livro);
-    resposta = {json: cadastra.json, statusCode: cadastra.statusCode, statusMessage:cadastra.statusMessage};
+
+    resposta = await BaseSteps.cadastraLivro(urlBook, livro);
+    BaseSteps.respostaOK(resposta, livro);
 
     expect(resposta.statusCode).to.be.eql(200);
     expect(resposta.statusMessage).to.be.eql('OK')
     expect(resposta.json.description).to.be.empty;
     expect(resposta.json).to.be.eql(livro);
 
-    let consulta = await BaseSteps.consultaLivro(urlBook, resposta.json.id)
-    resposta = {json: consulta.json, statusCode: consulta.statusCode, statusMessage: consulta.statusMessage};
+    resposta = await BaseSteps.consultaLivro(urlBook, resposta.json.id)
     BaseSteps.respostaOK(resposta, livro);
   });
 
   it('Cria um livro sem informar quantidade de página', async () => {
     livro.pageCount = 0;
-    let cadastra = await BaseSteps.cadastraLivro(urlBook, livro);
-    resposta = {json: cadastra.json, statusCode: cadastra.statusCode, statusMessage:cadastra.statusMessage};
+
+    resposta = await BaseSteps.cadastraLivro(urlBook, livro);
+    BaseSteps.respostaOK(resposta, livro);
 
     expect(resposta.statusCode).to.be.eql(200);
     expect(resposta.statusMessage).to.be.eql('OK')
     expect(resposta.json.pageCount).to.be.eq(0);
     expect(resposta.json).to.be.eql(livro);
 
-    let consulta = await BaseSteps.consultaLivro(urlBook, resposta.json.id)
-    resposta = {json: consulta.json, statusCode: consulta.statusCode, statusMessage: consulta.statusMessage};
+    resposta = await BaseSteps.consultaLivro(urlBook, resposta.json.id)
     BaseSteps.respostaOK(resposta, livro);
   });
 
